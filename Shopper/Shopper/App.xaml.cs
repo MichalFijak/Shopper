94ec02b7d78437af36cf9cc4c84b5ef9d568df2b
MichalFijak <michalfijak1@hotmail.com>
2025-05-14 19:35:45 +0200

    main program

---
+++ Shopper/Shopper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
